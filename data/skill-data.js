b37cdaf8d8fd1c5e6def640e3824b5d6ee7b0a68
User Name <user@email.com>
2022-06-15 22:46:18 -0400

    complete database

--- data/skill-data.js
+++
@@ -1,9 +0,0 @@
-const skills = [
-    {text: 'Feed llama', done: true, _id: 125223},
-    {text: 'Sleep under the stars', done: false, _id: 127904},
-    {text: 'Buy milk', done: false, _id: 139608},
-  ]
-  
-  export { 
-      skills
-  }
